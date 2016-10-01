c2b109c93c9825f814b68933348e0f11abde26fc
Christopher Wastl <christopher.wastl@edu.campus02.at>
2016-10-01 13:07:18 +0200

    NEU

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -3,9 +3,14 @@ public class HelloGitHub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int test = 0;
+		
+		System.out.println(test);
 		System.out.println("Hello GitHub!");
 		
-		//asdfdsd
+		
+		
 	}
 
 }
---
+++ src/OperatorsArithmetic.java
@@ -0,0 +1,16 @@
+
+public class OperatorsArithmetic {
+
+	public static void main(String[] args) {
+		
+		int a = 9;
+		int b = 5;
+		
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a % b);
+	}
+
+}
