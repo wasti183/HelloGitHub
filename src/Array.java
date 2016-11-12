d0420ea1d2fe9b4f4b6aadb8576b40d7c1b20628
Christopher Wastl <christopher.wastl@edu.campus02.at>
2016-11-12 09:04:12 +0100

    12.11.2016_Array

---
+++ src/Array.java
@@ -0,0 +1,20 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		
+		
+		int[] arrA = new int[3];
+				arrA[0] = 10;
+				arrA[1] = 5;
+				arrA[2] = 8;
+				
+		int [] arrB = {7, 1, 3};
+		
+		
+		System.out.println("+"+ arrA[0]);
+		System.out.println("+"+ arrB[0]);
+
+	}
+
+}
