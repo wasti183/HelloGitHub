f8c5a2e3f0fcc3132aeb4b001afb84dc180a43ef
Christopher Wastl <christopher.wastl@edu.campus02.at>
2016-11-12 09:17:21 +0100

    12.11.2016 Array UE11

---
+++ src/Uebung11Array.java
@@ -0,0 +1,29 @@
+
+public class Uebung11Array {
+
+	public static void main(String[] args) {
+		
+
+		String[] month = new String[12];
+		month[0] = "Jaenner";
+		month[1] = "Februar";
+		month[2] = "Maerz";
+		month[3] = "April";
+		month[4] = "Mai";
+		month[5] = "Juni";
+		month[6] = "Juli";
+		month[7] = "August";
+		month[8] = "September";
+		month[9] = "Oktober";
+		month[10] = "November";
+		month[11] = "Dezember";
+		
+			for(int m =0; m < month.length;m++){
+				System.out.println(month[m]+ "["+(m+1)+"]");
+				
+			}
+		}
+		
+	}
+
+
